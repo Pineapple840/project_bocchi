dcdc474d281309bfe58175b94a644436e51f5913
Pineapple <finnaggarwal@gmail.com>
2025-09-02 20:23:30 +0100

    use ogv file for web

--- levels/ffmpeg_video_stream.gd
+++ levels/ffmpeg_video_stream.gd
@@ -11,15 +11,17 @@ func _ready():
 	
 
 func PlayVideo(video_start_time: float, seconds_per_beat: float, offset: float, jump_distance: float):
-	var stream = FFmpegVideoStream.new()
-	stream.set_file("res://videos/wasurete_yaranai.mp4")
+	
+	#var stream = FFmpegVideoStream.new()
+	var stream = VideoStreamTheora.new()
+	stream.set_file("res://videos/wasurete_yaranai.ogv")
 
 	
 	video_player.stream = stream
 	script_video_start_time = video_start_time
 	#video_player.playback_speed = 2.0
 	video_player.play()
-	video_player.set_stream_position(video_start_time)
+	#video_player.set_stream_position(video_start_time)
 	print(video_player.get_stream_position())
 	#await(video_s == true)
 	#print(video_player.get_stream_position())
